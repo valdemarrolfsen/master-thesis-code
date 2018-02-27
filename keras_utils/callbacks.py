f423f650fcb4dbcd7f20fba335889dd6e541464a
Ruben Schmidt <rubschmi@gmail.com>
2018-02-27 13:25:33 +0100

    fix: tensor logging

--- keras_utils/callbacks.py
+++ keras_utils/callbacks.py
@@ -23,7 +23,7 @@ class LrReducer(Callback):
 def callbacks(logdir):
     model_checkpoint = ModelCheckpoint("weights_train/weights.{epoch:02d}-{loss:.2f}.h5", monitor='loss', verbose=1,
                                        period=10)
-    tensorboard_callback = TensorBoard(log_dir=logdir, write_graph=False, write_images=True, histogram_freq=1)
+    # tensorboard_callback = TensorBoard(log_dir=logdir, write_graph=False, write_images=True, histogram_freq=1)
     plateau_callback = ReduceLROnPlateau(monitor='loss', factor=0.99, verbose=1, patience=0, min_lr=0.00001)
     # return [CheckPoints(), tensorboard_callback, LrReducer()]
-    return [model_checkpoint, tensorboard_callback, plateau_callback, LrReducer()]
+    return [model_checkpoint, plateau_callback, LrReducer()]
