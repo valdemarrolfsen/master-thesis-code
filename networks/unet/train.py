d35e9b77888983e286b74cbb833697c6d5134f80
Ruben Schmidt <rubschmi@gmail.com>
2018-03-01 17:14:04 +0100

    add validation

--- networks/unet/train.py
+++ networks/unet/train.py
@@ -8,11 +8,11 @@ from networks.unet.unet import build_unet
 def train_unet(data_dir, logdir, input_size, nb_classes, batch_size, initial_epoch, steps_per_epoch):
     model = build_unet(nb_classes, input_size[0], input_size[1], 3)
     train_generator = create_generator(os.path.join(data_dir, 'train'), input_size, batch_size, nb_classes)
-    # val_generator = create_generator(os.path.join(data_dir, 'val'), input_size, batch_size, nb_classes, rescale=False)
+    val_generator = create_generator(os.path.join(data_dir, 'val'), input_size, batch_size, nb_classes, rescale=False)
     model.fit_generator(
         generator=train_generator,
-        # validation_data=val_generator,
-        # validation_steps=2,
+        validation_data=val_generator,
+        validation_steps=900,
         epochs=100, verbose=True, steps_per_epoch=steps_per_epoch,
         callbacks=callbacks(logdir), initial_epoch=initial_epoch)
 
