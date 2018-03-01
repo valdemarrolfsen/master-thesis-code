a7b8334c0205c203b505b65c842ebe905c23f36d
Ruben Schmidt <rubschmi@gmail.com>
2018-03-01 18:50:41 +0100

    change histogram freq to 0

--- keras_utils/callbacks.py
+++ keras_utils/callbacks.py
@@ -5,6 +5,6 @@ from keras.callbacks import Callback, ReduceLROnPlateau, ModelCheckpoint, Tensor
 def callbacks(logdir):
     model_checkpoint = ModelCheckpoint("weights_train/weights.{epoch:02d}-{loss:.2f}.h5", monitor='loss', verbose=1,
                                        period=1)
-    tensorboard_callback = TensorBoard(log_dir=logdir, write_graph=True, write_images=True, histogram_freq=1)
+    tensorboard_callback = TensorBoard(log_dir=logdir, write_graph=True, write_images=True, histogram_freq=0)
     plateau_callback = ReduceLROnPlateau(monitor='loss', factor=0.99, verbose=1, patience=0, min_lr=0.00001)
     return [model_checkpoint, plateau_callback, tensorboard_callback]
