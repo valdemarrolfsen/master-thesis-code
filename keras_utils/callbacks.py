2f31fa7ca92f7b18b8b928b11a1ccd6a77930608
Ruben Schmidt <rubschmi@gmail.com>
2018-03-01 19:40:00 +0100

    added early stop callback

--- keras_utils/callbacks.py
+++ keras_utils/callbacks.py
@@ -1,10 +1,10 @@
-import keras.backend as K
-from keras.callbacks import Callback, ReduceLROnPlateau, ModelCheckpoint, TensorBoard
+from keras.callbacks import ReduceLROnPlateau, ModelCheckpoint, TensorBoard, EarlyStopping
 
 
 def callbacks(logdir):
     model_checkpoint = ModelCheckpoint("weights_train/weights.{epoch:02d}-{loss:.2f}.h5", monitor='loss', verbose=1,
                                        period=1)
-    tensorboard_callback = TensorBoard(log_dir=logdir, write_graph=True, write_images=True, histogram_freq=0)
+    tensorboard_callback = TensorBoard(log_dir=logdir, write_graph=True, histogram_freq=0)
     plateau_callback = ReduceLROnPlateau(monitor='loss', factor=0.99, verbose=1, patience=0, min_lr=0.00001)
-    return [model_checkpoint, plateau_callback, tensorboard_callback]
+    early_stopping = EarlyStopping(monitor='val_loss', patience=2, verbose=1)
+    return [model_checkpoint, plateau_callback, tensorboard_callback, early_stopping]
