38de0e3e8387800ca23339f543d8919a2e87fde4
Valdemar Rolfsen <valdemar.rolfsen@gmail.com>
2018-03-01 09:58:24 +0100

    Fixed generator with one hot encoding

--- keras_utils/generators.py
+++ keras_utils/generators.py
@@ -1,33 +1,10 @@
 import os
 import numpy as np
-from keras.preprocessing import image
 from keras.preprocessing.image import ImageDataGenerator
 from keras.utils import to_categorical
 
 
-def extract_imagelist(image_dir, input_size):
-    image_dir = image_dir + '/buildings'
-
-    image_files = []
-
-    for entry in os.listdir(image_dir):
-        file_path = "{}/{}".format(image_dir, entry)
-        if os.path.isfile(file_path) and file_path.lower().endswith(('.png', '.jpg', '.tif')):
-            image_files.append(file_path)
-
-    image_len = len(image_files)
-    all_images = np.empty([image_len, input_size, input_size, 3])
-
-    for i in range(image_len):
-        img = image.load_img(image_files[i], target_size=(input_size, input_size))
-        img = image.img_to_array(img)
-        img = np.expand_dims(img, axis=0)
-        all_images[i, :, :, :] = img
-
-    return all_images
-
-
-def create_generator(datadir='', input_size=(713, 713), batch_size=32):
+def create_generator(datadir = '', input_size=(713, 713), batch_size=32):
     image_dir = os.path.join(datadir, "examples")
     label_dir = os.path.join(datadir, "labels")
 
@@ -57,20 +34,10 @@ def create_generator(datadir='', input_size=(713, 713), batch_size=32):
     image_datagen = ImageDataGenerator(**datagen_args)
     label_datagen = ImageDataGenerator(**datagen_args)
 
-    images = extract_imagelist(image_dir, input_size[0])
-    labels = extract_imagelist(label_dir, input_size[0])
-
-    print(labels)
-
-    labels = to_categorical(labels)
-
     # Compute quantities required for featurewise normalization
     # (std, mean, and principal components if ZCA whitening is applied).
     # Use the same seed for both generators so they return corresponding images
     seed = 1
-
-    image_datagen.fit(images, augment=True, seed=seed)
-    label_datagen.fit(labels, augment=True, seed=seed)
 
     image_generator = image_datagen.flow_from_directory(
         image_dir,
@@ -84,7 +51,18 @@ def create_generator(datadir='', input_size=(713, 713), batch_size=32):
         batch_size=batch_size,
         target_size=input_size,
         class_mode=None,
+        color_mode='grayscale',
         seed=seed)
 
     generator = zip(image_generator, label_generator)
-    return generator
+    return custom_gen(generator)
+
+
+def custom_gen(generator):
+    for img, mask in generator:
+        output = np.ndarray((2, 473, 473, 256))
+
+        for i in range(mask.shape[0]):
+            output[i] = to_categorical(mask[i], num_classes=256)
+
+        yield img, output
