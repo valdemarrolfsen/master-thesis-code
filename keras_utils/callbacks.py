49c8edc584e6b966591a4ac663a0492322ed192f
Ruben Schmidt <rubschmi@gmail.com>
2018-03-06 17:35:41 +0100

    only save best for model checkpoint, monitor val_acc

--- keras_utils/callbacks.py
+++ keras_utils/callbacks.py
@@ -2,9 +2,10 @@ from keras.callbacks import ReduceLROnPlateau, ModelCheckpoint, TensorBoard, Ear
 
 
 def callbacks(logdir):
-    model_checkpoint = ModelCheckpoint("weights_train/weights.{epoch:02d}-{loss:.2f}.h5", monitor='loss', verbose=1,
-                                       period=1)
+    checkpoint = ModelCheckpoint("weights_train/weights.{epoch:02d}-{loss:.2f}.h5", monitor='val_acc', verbose=2,
+                                 save_best_only=True, save_weights_only=False, mode='max')
+
     tensorboard_callback = TensorBoard(log_dir=logdir, write_graph=True, histogram_freq=0)
     plateau_callback = ReduceLROnPlateau(monitor='loss', factor=0.99, verbose=1, patience=0, min_lr=0.00001)
     early_stopping = EarlyStopping(monitor='val_acc', patience=4, verbose=1)
-    return [model_checkpoint, plateau_callback, tensorboard_callback, early_stopping]
+    return [checkpoint, plateau_callback, tensorboard_callback, early_stopping]
