32440d3e4a82c16a0334b8cf1c194ef32412101b
Ruben Schmidt <rubschmi@gmail.com>
2018-03-09 16:51:55 +0100

    increase epochs

--- networks/unet/train.py
+++ networks/unet/train.py
@@ -19,6 +19,6 @@ def train_unet(data_dir, logdir, weights_dir, input_size, nb_classes, batch_size
         validation_data=val_generator,
         validation_steps=val_samples//batch_size,
         steps_per_epoch=num_samples//batch_size,
-        epochs=100, verbose=True,
+        epochs=10000, verbose=True,
         callbacks=callbacks(logdir, weightsdir=weights_dir), initial_epoch=initial_epoch)
 
