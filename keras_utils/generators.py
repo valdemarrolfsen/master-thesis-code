9fecf56a690f4de07b141fc100e3043e96c85efc
Valdemar Rolfsen <valdemar.rolfsen@gmail.com>
2018-03-09 16:09:06 +0100

    Fixed bug in generator for providing file names

--- keras_utils/generators.py
+++ keras_utils/generators.py
@@ -88,10 +88,22 @@ def create_generator(datadir, input_size, batch_size, nb_classes, rescale=False,
         seed=seed)
 
     generator = zip(image_generator, label_generator)
-    return custom_gen(generator, input_size, batch_size, nb_classes, with_file_names), image_generator.samples
+
+    file_names = None
+
+    if with_file_names:
+        file_names = image_generator.filenames
+
+    return custom_gen(
+        generator,
+        input_size,
+        batch_size,
+        nb_classes,
+        file_names
+    ), image_generator.samples
 
 
-def custom_gen(generator, input_size, batch_size, nb_classes, with_file_names):
+def custom_gen(generator, input_size, batch_size, nb_classes, file_names):
     while True:
         img, mask = next(generator)
 
@@ -103,9 +115,9 @@ def custom_gen(generator, input_size, batch_size, nb_classes, with_file_names):
         for i in range(mask.shape[0]):
             output[i] = to_categorical(mask[i], num_classes=nb_classes)
 
-        if with_file_names:
+        if file_names:
             idx = (generator.batch_index - 1) * generator.batch_size
-            file_names = generator.filenames[idx: idx + generator.batch_size]
+            file_names = file_names[idx: idx + generator.batch_size]
             yield img, output, file_names
         else:
             yield img, output
