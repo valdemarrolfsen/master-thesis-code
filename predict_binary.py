87b5fd0c26af098428c5072dda353f4fcb9e1a71
Ruben Schmidt <ruben@spacemaker.ai>
2018-03-22 10:32:14 +0100

    add model names to predict

--- predict_binary.py
+++ predict_binary.py
@@ -5,7 +5,7 @@ import numpy as np
 
 from keras_utils.generators import create_generator
 from keras_utils.prediction import get_real_image, get_geo_frame, geo_reference_raster
-from networks.unet_binary.unet import build_unet_binary_deeper_elu
+from networks.unet_binary.unet import build_unet_binary_deeper_elu, build_unet_binary_standard
 
 
 def run():
@@ -16,7 +16,7 @@ def run():
     parser.add_argument("--output-path", type=str, default="")
     parser.add_argument("--input-size", type=int, default=713)
     parser.add_argument("--batch-size", type=int, default=713)
-    parser.add_argument("--model-name", type=str, default="")
+    parser.add_argument("--model-name", type=str, default="standard")
     args = parser.parse_args()
 
     model_name = args.model_name
@@ -25,7 +25,8 @@ def run():
     batch_size = args.batch_size
 
     model_choices = {
-        'unet': build_unet_binary_deeper_elu,
+        'deeper-elu': build_unet_binary_deeper_elu,
+        'standard': build_unet_binary_standard,
     }
 
     model_choice = model_choices[model_name]
