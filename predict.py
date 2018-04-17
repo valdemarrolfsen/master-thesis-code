be4ccaea7ce3075273368fb72db4fff7f054ac54
Ruben Schmidt <ruben@spacemaker.ai>
2018-04-17 12:04:30 +0200

    test clas jaccard

--- predict.py
+++ predict.py
@@ -43,7 +43,7 @@ def run(args):
     probs = model.predict(images, verbose=1)
 
     IOU = []
-    other_IOU = class_jaccard(K.variable(masks), K.variable(probs))
+    other_IOU = K.eval(class_jaccard(K.variable(masks), K.variable(probs)))
     for i, prob in enumerate(probs):
         result = np.argmax(prob, axis=2)
         mask_result = np.argmax(masks[i], axis=2)
