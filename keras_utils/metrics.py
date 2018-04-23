537b3feab5790797b6a9af1355a92c579ef7aca4
Ruben Schmidt <ruben@spacemaker.ai>
2018-04-23 12:40:43 +0200

    new binary jacc loss

--- keras_utils/metrics.py
+++ keras_utils/metrics.py
@@ -40,12 +40,20 @@ def jaccard_distance(y_true, y_pred):
     return jac
 
 
+def binary_jaccard_distance(y_true, y_pred):
+    smooth = K.epsilon()
+    intersection = K.sum(K.abs(y_true * y_pred))
+    union = K.sum(K.abs(y_true) + K.abs(y_pred)) - intersection
+    jac = (intersection + smooth) / (union + smooth)
+    return jac
+
+
 def soft_jaccard_loss(y_true, y_pred):
     return -K.log(jaccard_distance(y_true, y_pred)) + K.categorical_crossentropy(y_true, y_pred)
 
 
 def binary_jaccard_loss(target, output):
-    return 1 - jaccard_distance(target, output)
+    return 1 - binary_jaccard_distance(target, output)
 
 
 def binary_soft_jaccard_loss(target, output):
