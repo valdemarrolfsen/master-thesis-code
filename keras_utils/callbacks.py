b1f3eae7fe42241acb8007a1fd646c9866b75b95
Ruben Schmidt <ruben@spacemaker.ai>
2018-04-24 10:03:23 +0200

    feat: Mean IOU callback

--- keras_utils/callbacks.py
+++ keras_utils/callbacks.py
@@ -1,6 +1,25 @@
 import numpy as np
 import os
-from keras.callbacks import ReduceLROnPlateau, ModelCheckpoint, TensorBoard, EarlyStopping
+from keras.callbacks import ReduceLROnPlateau, ModelCheckpoint, TensorBoard, EarlyStopping, Callback
+
+from keras_utils.metrics import batch_general_jaccard
+
+
+class MeanIOUCallback(Callback):
+    def __init__(self):
+        super().__init__()
+        self.ious = []
+
+    def on_train_begin(self, logs=None):
+        pass
+
+    def on_epoch_end(self, epoch, logs=None):
+        val_predict = np.asarray(self.model.predict(self.model.validation_data[0]))
+        val_target = self.model.validation_data[1]
+        meaniou = np.mean(batch_general_jaccard(val_target, val_predict, binary=True))
+        self.ious.append(meaniou)
+        print('- mean IOU: {}'.format(meaniou))
+        return
 
 
 def callbacks(logdir, weightsdir, filename, monitor_val='val_acc'):
@@ -11,4 +30,5 @@ def callbacks(logdir, weightsdir, filename, monitor_val='val_acc'):
     tensorboard_callback = TensorBoard(log_dir=logdir, write_graph=True, histogram_freq=0)
     plateau_callback = ReduceLROnPlateau(monitor=monitor_val, factor=np.sqrt(0.1), verbose=1, patience=3, min_lr=0.5e-6)
     early_stopping = EarlyStopping(monitor=monitor_val, patience=10, verbose=1)
-    return [checkpoint, plateau_callback, tensorboard_callback, early_stopping]
+    meaniou = MeanIOUCallback()
+    return [checkpoint, plateau_callback, tensorboard_callback, early_stopping, meaniou]
