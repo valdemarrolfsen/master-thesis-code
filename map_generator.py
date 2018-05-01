58b8a6e8a5ec17b6c6d4dad35c63e3ae566d1a82
Ruben Schmidt <ruben@spacemaker.ai>
2018-05-01 18:42:23 +0200

    started on map generator again with smooth tiled pred

--- map_generator.py
+++ map_generator.py
@@ -1,8 +1,13 @@
 import argparse
 import cv2
 import numpy as np
+from tqdm import tqdm
+from keras.optimizers import Adam
 from keras.preprocessing.image import ImageDataGenerator
 
+from keras_utils.generators import load_images_from_folder
+from keras_utils.losses import binary_soft_jaccard_loss
+from keras_utils.metrics import binary_jaccard_distance_rounded
 from keras_utils.smooth_tiled_predictions import predict_img_with_smooth_windowing
 from networks.pspnet.net_builder import build_pspnet
 from networks.unet.unet import build_unet
@@ -34,7 +39,6 @@ def get_generator(images):
 
     image_datagen = ImageDataGenerator(**datagen_args)
     image_datagen.fit(images)
-
     return image_datagen
 
 
@@ -51,27 +55,18 @@ def image_to_neural_input(images, image_datagen):
 def run():
     parser = argparse.ArgumentParser()
     parser.add_argument("--weights-path", type=str)
-    parser.add_argument("--epoch-number", type=int, default=5)
     parser.add_argument("--test-images", type=str, default="")
-    parser.add_argument("--sample-path", type=str, default="")
+    parser.add_argument("--sample-images", type=str, default="")
     parser.add_argument("--output-path", type=str, default="")
     parser.add_argument("--input-size", type=int, default=713)
     parser.add_argument("--model-name", type=str, default="")
     parser.add_argument("--classes", type=int)
 
-    class_color_map = {
-        0: [237, 237, 237],  # Empty
-        1: [254, 241, 179],  # Roads
-        2: [116, 173, 209],  # Water
-        3: [193, 235, 176],  # Grass
-        4: [170, 170, 170]  # Buildings
-    }
-
     args = parser.parse_args()
-
     n_classes = args.classes
     model_name = args.model_name
     images_path = args.test_images
+    sample_path = args.sample_images
     input_size = args.input_size
 
     model_choices = {
@@ -80,40 +75,32 @@ def run():
     }
 
     model_choice = model_choices[model_name]
-    model = model_choice(n_classes, (input_size, input_size))
+    model = model_choice((input_size, input_size), 1)
+    model.compile(
+        optimizer=Adam(lr=1e-4),
+        loss=binary_soft_jaccard_loss,
+        metrics=['acc', binary_jaccard_distance_rounded])
     model.load_weights(args.weights_path)
 
-    images = [cv2.imread(images_path)]
-    generator = get_generator(images)
+    sample_images = np.array(load_images_from_folder(sample_path, num_samples=500))
+    generator = get_generator(sample_images)
+    sample_images = None
+    # load all images
+    images = load_images_from_folder(images_path, num_samples=100000)
 
-    for i, input_img in enumerate(images):
-
-        pred = predict_img_with_smooth_windowing(
-            input_img,
-            window_size=input_size,
-            subdivisions=2,  # Minimal amount of overlap for windowing. Must be an even number.
-            nb_classes=n_classes,
-            pred_func=(
-                lambda img_batch_subdiv: model.predict(
-                    image_to_neural_input(img_batch_subdiv, generator), verbose=True
-                )
+    pred = predict_img_with_smooth_windowing(
+        images[0],
+        window_size=input_size,
+        subdivisions=2,  # Minimal amount of overlap for windowing. Must be an even number.
+        nb_classes=n_classes,
+        pred_func=(
+            lambda img_batch_subdiv: model.predict(
+                image_to_neural_input(img_batch_subdiv, generator), verbose=True
             )
         )
+    )
 
-        result = np.argmax(pred, axis=2)
-
-        seg_img = np.zeros((pred.shape[0], pred.shape[1], 3))
-
-        for c in range(n_classes):
-            seg_img[:, :, 0] += ((result[:, :] == c) * (class_color_map[c][2])).astype('uint8')
-            seg_img[:, :, 1] += ((result[:, :] == c) * (class_color_map[c][1])).astype('uint8')
-            seg_img[:, :, 2] += ((result[:, :] == c) * (class_color_map[c][0])).astype('uint8')
-
-        mask_name = "pred-{}.tif".format(i)
-        img_name = "img-{}.tif".format(i)
-
-        cv2.imwrite("{}/{}".format(args.output_path, mask_name), seg_img)
-        cv2.imwrite("{}/{}".format(args.output_path, img_name), input_img)
+    print(pred)
 
 
 if __name__ == '__main__':
