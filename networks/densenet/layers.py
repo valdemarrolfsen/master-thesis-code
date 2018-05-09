a9e0f21e0afd327abb4579ffbc8cf9f48d2eb94c
Ruben Schmidt <ruben@spacemaker.ai>
2018-05-09 21:03:51 +0200

    subpixel upscaling instead of decon

--- networks/densenet/layers.py
+++ networks/densenet/layers.py
@@ -1,3 +1,4 @@
+import tensorflow as tf
 from keras.engine import Layer
 from keras.utils.conv_utils import normalize_data_format
 
@@ -51,7 +52,7 @@ class SubPixelUpscaling(Layer):
         pass
 
     def call(self, x, mask=None):
-        y = K.depth_to_space(x, self.scale_factor, self.data_format)
+        y = tf.depth_to_space(x, self.scale_factor, self.data_format)
         return y
 
     def compute_output_shape(self, input_shape):
