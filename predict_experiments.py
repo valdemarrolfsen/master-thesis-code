a027fd9ba238e1e736ca81e0872b9b956b852d4b
Valdemar Rolfsen <valdemar.rolfsen@gmail.com>
2018-05-10 11:15:07 +0200

    Fixed predict for exp

---
+++ predict_experiments.py
@@ -0,0 +1,44 @@
+import numpy as np
+from keras import backend as K
+
+from keras_utils.generators import create_generator
+from keras_utils.metrics import batch_general_jaccard, f1_score
+from networks.unet.unet import build_unet
+
+from run_unet_experiments import experiments
+
+
+def run():
+    images_path = '/mnt/ekstern/rubval/buildings/test'
+    input_size = 320
+    batch_size = 2620
+    weights = 'weights_train/weights.unet-experiment-{}.h5'
+
+    generator, _ = create_generator(
+        images_path,
+        (input_size, input_size),
+        batch_size,
+        1,
+        rescale=False,
+        binary=True
+    )
+
+    images, masks, file_names = next(generator)
+
+    for i, options in enumerate(experiments):
+        print('Running prediction for experiment {}'.format(str(i)))
+        dropout = options['dropout']
+
+        model = build_unet(input_size, nb_classes=1, dropout=dropout)
+        model.load_weights(weights.format(i))
+
+        probs = model.predict(images, verbose=1)
+
+        iou = batch_general_jaccard(masks, probs, binary=True)
+        f1 = K.eval(f1_score(K.variable(masks), K.variable(probs)))
+        print('mean IOU for {}: {}'.format(i, np.mean(iou)))
+        print('F1 score for {}: {}'.format(i, f1))
+
+
+if __name__ == '__main__':
+    run()
