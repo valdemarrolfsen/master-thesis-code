e7a440302cfd181d4adda7cc0c3c26ed36ef7014
Valdemar Rolfsen <valdemar.rolfsen@gmail.com>
2018-05-10 11:23:43 +0200

    Small fix

--- predict_experiments.py
+++ predict_experiments.py
@@ -10,7 +10,7 @@ from run_unet_experiments import experiments
 
 def run():
     images_path = '/mnt/ekstern/rubval/buildings/test'
-    input_size = 320
+    input_size = (320, 320)
     batch_size = 2622
     weights = 'weights_train/weights.unet-experiment-{}.h5'
 
