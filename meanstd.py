922b1b02d25d6b07d6b16485b61ebfa5c1124fcb
Ruben Schmidt <ruben@spacemaker.ai>
2018-05-15 20:22:07 +0200

    mean

--- meanstd.py
+++ meanstd.py
@@ -1,6 +1,5 @@
 import os
 import cv2
-import random
 import numpy as np
 from tqdm import tqdm
 
@@ -10,17 +9,16 @@ imdir = '/mnt/ekstern/rubval/buildings/{}/examples/buildings/'
 
 def run():
     ims = []
-    files = []
     for folder in folders:
         path = imdir.format(folder)
-        files += os.listdir(path)
-    files = random.sample(files, 5000)
-    for file in tqdm(files):
-        path = imdir.format(folder)
-        im = os.path.join(path, file)
-        img = cv2.imread(im)
-        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-        ims.append(img / 255)
+        files = os.listdir(path)
+        for i, file in enumerate(tqdm(files)):
+            if i > 3000:
+                break
+            im = os.path.join(path, file)
+            img = cv2.imread(im)
+            img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+            ims.append(img / 255)
     print(np.mean(ims, axis=(0, 1, 2)))
     print(np.std(ims, axis=(0, 1, 2)))
 
