fb7b024800c83e0d7dcd2472cff45b40bfbd4d8e
Valdemar Rolfsen <valdemar.rolfsen@gmail.com>
2018-05-15 21:52:52 +0200

    Small change

--- meanstd.py
+++ meanstd.py
@@ -12,13 +12,13 @@ imdir = '/mnt/ekstern/rubval/buildings/{}/examples/buildings/'
 
 def run():
     means = []
-    for i in range(100):
+    for i in tqdm(range(100)):
         ims = []
         for folder in folders:
             path = imdir.format(folder)
             files = os.listdir(path)
             files = random.sample(files, 20)
-            for i, file in enumerate(tqdm(files)):
+            for i, file in enumerate(files):
                 im = os.path.join(path, file)
                 img = cv2.imread(im)
                 img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
