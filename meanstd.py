8a7020dafd3ac250a02bcef71016e929bbfeeddf
Ruben Schmidt <ruben@spacemaker.ai>
2018-05-15 20:38:43 +0200

    mean

--- meanstd.py
+++ meanstd.py
@@ -3,6 +3,7 @@ import gc
 import os
 import cv2
 import numpy as np
+import random
 from tqdm import tqdm
 
 folders = ['train', 'val', 'test']
@@ -14,9 +15,8 @@ def run():
     for folder in folders:
         path = imdir.format(folder)
         files = os.listdir(path)
+        files = random.sample(files, 2000)
         for i, file in enumerate(tqdm(files)):
-            if i > 1000:
-                break
             im = os.path.join(path, file)
             img = cv2.imread(im)
             img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
