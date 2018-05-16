97d5b862e4ec19cd27b9c0732f9004cd19d8c0a0
Valdemar Rolfsen <valdemar.rolfsen@gmail.com>
2018-05-17 00:58:07 +0200

    Small fix

--- run_finetune.py
+++ run_finetune.py
@@ -75,10 +75,12 @@ def run():
                 mean=np.array([[[0.36654497, 0.35386439, 0.30782658]]]),
                 std=np.array([[[0.19212837, 0.19031791, 0.18903286]]])
             )
+
             if run['network'] == 'unet':
                 model = build_unet(input_size, nb_classes=1)
             else:
                 model = build_densenet(input_size, 1, 67)
+
             model.summary()
             gpus = get_number_of_gpus()
             print('Fund {} gpus'.format(gpus))
