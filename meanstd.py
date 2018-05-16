a83efa54d2cf585a0baacf0d2ab9e448bc34d924
Ruben Schmidt <ruben@spacemaker.ai>
2018-05-17 00:43:40 +0200

    remove deletion code

--- meanstd.py
+++ meanstd.py
@@ -1,48 +1,10 @@
-import gc
-from queue import Queue
-
-import os
 import cv2
 import numpy as np
-import random
-import threading
+import os
 from tqdm import tqdm
-
-from preprocessing import utils
-from preprocessing.utils import contains_zero_value
 
 folders = ['bergen', 'bodo', 'oslo', 'stavanger', 'tromso', 'trondheim']
 imdir = '/mnt/ekstern/rubval/{}/tiled-512x512/'
-
-
-def delete():
-    files = []
-    for folder in folders:
-        path = imdir.format(folder)
-        files_paths = os.listdir(path)
-        absolute_paths = [os.path.join(path, file) for file in files_paths]
-        files = files + absolute_paths
-    print("Found {} files".format(len(files)))
-
-    q = Queue()
-    for i, file in enumerate(files):
-        q.put((file, i))
-
-    for i in range(8):
-        t = threading.Thread(target=work, args=(q, len(files)))
-        # Sticks the thread in a list so that it remains accessible
-        t.daemon = True
-        t.start()
-
-    q.join()
-
-
-def work(q, nb_files):
-    while not q.empty():
-        utils.print_process(nb_files - q.qsize(), nb_files)
-        file, i = q.get()
-        if contains_zero_value(file):
-            os.remove(file)
 
 
 def run():
