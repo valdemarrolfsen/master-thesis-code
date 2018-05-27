776a41378263dd41c2b30fac6464a6e9c72306f2
Ruben Schmidt <ruben@spacemaker.ai>
2018-05-27 17:45:37 +0200

    dense

--- map_generator.py
+++ map_generator.py
@@ -93,23 +93,23 @@ def run():
     generator = get_generator()
     # load the image
     image = np.array(Image.open(image_path))
-    pred = predict_img_with_smooth_windowing(
-        image,
-        window_size=input_size,
-        subdivisions=2,  # Minimal amount of overlap for windowing. Must be an even number.
-        nb_classes=1,
-        pred_func=(
-            lambda img_batch_subdiv: model.predict(
-                image_to_neural_input(img_batch_subdiv, generator), verbose=True
-            )
-        )
-    )
-    pred = np.round(pred)
-    pred = (pred[:, :, 0] * 255.).astype(np.uint8)
-    out_path = os.path.join(output_path, output_name)
-    print(cv2.imwrite(out_path, pred))
+    # pred = predict_img_with_smooth_windowing(
+    #     image,
+    #     window_size=input_size,
+    #     subdivisions=2,  # Minimal amount of overlap for windowing. Must be an even number.
+    #     nb_classes=1,
+    #     pred_func=(
+    #         lambda img_batch_subdiv: model.predict(
+    #             image_to_neural_input(img_batch_subdiv, generator), verbose=True
+    #         )
+    #     )
+    # )
+    # pred = np.round(pred)
+    # pred = (pred[:, :, 0] * 255.).astype(np.uint8)
+    # out_path = os.path.join(output_path, output_name)
+    # print(cv2.imwrite(out_path, pred))
 
-    cheap = cheap_tiling_prediction(image, window_size=input_size, nb_classes=1, pred_func=(
+    cheap = cheap_densenet_tiling_prediction(image, window_size=input_size, nb_classes=1, pred_func=(
         lambda img_batch_subdiv: model.predict(
             image_to_neural_input(np.array(img_batch_subdiv), generator), verbose=True
         )
